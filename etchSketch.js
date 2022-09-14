a1fb67663eff5d82d446fc1b12b78d88e89ec7ea
hao-codes <59805422+hao-codes@users.noreply.github.com>
2022-09-14 23:27:08 +0200

    fixed clear button, added form to enter gridsize, changed layout

--- etchSketch.js
+++ etchSketch.js
@@ -1,61 +1,102 @@
-//const grid = document.getElementsByClassName("grid");
-//console.log(grid);
-const div = document.createElement('squareDiv');
-//div.setAttribute("background: black");
-
-const reset = document.getElementById("reset")
-let gridsize = 10;
-
-const grid = document.querySelector(".grid");
-let percentage = (100 / gridsize).toFixed(2) ;
-console.log(percentage);
 // create square divs
-//create 16x16 grid of these divs
+// create 16x16 grid of these divs
 // add hover effect: color of grid changes when mouse hovers over them
 // add button, asking for size of grid in the same space, max = 100, for a new sketch pad
 
 
-function is_dom_element(obj) {
-    return !!(obj && obj.nodeType === 1);
-  };
+const div = document.createElement('squareDiv');
+//div.setAttribute("background: black");
 
+const clearButton = document.getElementById("clear")
+
+const btn_size = document.getElementById("btn-sizing");
+console.log(btn_size);
+
+const grid = document.querySelector(".grid");
+
+//btn_size.addEventListener("click", (event) => )
+
+const gridsizeSubmit = document.querySelector(".gridsizeSubmit");
+const guessField = document.querySelector('.gridsize');
+
+const currentGridsize = document.getElementById("currentGridsize");
+console.log(currentGridsize);
+
+console.log(typeof gridsize === 'undefined');
+
+if (typeof gridsize === 'undefined') {
+  let gridsize = 10;
+  generateDivs(gridsize);
+  currentGridsize.innerHTML = gridsize.toString() + " x " + gridsize.toString();
+}
+//currentGridsize.innerHTML = gridsize.toString() + " x " + gridsize.toString();
+
+function checkGridsize() {
+
+  let gridsize = Number(guessField.value)
+
+  console.log("Gridsize :" + gridsize.toString());
+
+
+  if (Number.isInteger(gridsize) == false || gridsize > 100 || gridsize < 1) {
+    alert("Invalid input, please enter a number between 1 and 100");
+  } else {
+
+    //console.log(percentage);
+    grid.innerHTML = '';
+
+    generateDivs(gridsize);
+    currentGridsize.innerHTML = gridsize.toString() + " x " + gridsize.toString();
+  }
+
+};
+
+
+// set gridsize and check if size is in [1, 100]
+gridsizeSubmit.addEventListener('click', checkGridsize);
 
 
 //function for creating grid of divs
+
 function generateDivs(size) {
-    //var body = document.body;
-    //const grid = document.querySelector(".grid");
-    //console.log(is_dom_element(grid));
-    for (let i = 0; i < size; i++) {
-        let row = document.createElement("div");
-        
-        //console.log(percentage);
-        row.className = "row";
-    
-        for (let j = 1; j <= size; j++) {
-            let cell = document.createElement("div");
-            cell.className = "cell";
-            cell.innerText = " ";
-            //cell.setAttribute('style','background')
-            cell.style.backgroundColor = "white";
-            cell.style.width =  percentage.toString() + '%';
-            
-            //cell.style.height = percentage.toString() + '%';
-          //  console.log(cell.style.width);
-            row.appendChild(cell);
-            };
-        grid.appendChild(row);
-        };
-   
+
+  let percentage = (100 / size).toFixed(2);
+  //console.log(percentage);
+
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+
+    //console.log(percentage);
+    row.className = "row";
+
+    for (let j = 1; j <= size; j++) {
+      let cell = document.createElement("div");
+      cell.className = "cell";
+      cell.innerText = " ";
+      //cell.setAttribute('style','background')
+      cell.style.backgroundColor = "white";
+      cell.style.width = percentage.toString() + '%';
+
+      row.appendChild(cell);
+    };
+    grid.appendChild(row);
+  };
+
 }
 
-generateDivs(gridsize);
-//document.getElementById("grid").innerText = e.innerHTML;
 
-grid.addEventListener('mouseover', (event)=> event.target.style.backgroundColor = "blue");
+grid.addEventListener('mouseover', (event) => event.target.style.backgroundColor = "#c1afb7");
 
-reset.addEventListener('click', function(e) {
-  e.target.style.backgroundColor = "white"
+// button for clearing grid back to white
+clearButton.addEventListener('click', function onClick(event) {
+  const cells = document.querySelectorAll(".cell");
+  // console.log(cells);
+  document.querySelectorAll(".grid")[0].style.backgroundColor = "white";
+  for (let k = 0; k < cells.length; k++) {
+    cells[k].style.backgroundColor = "white";
+  };
 });
 
-//reset.addEventListener('click', () => { alert("Hello World"), grid.style.backgroundColor = "white"});
+function is_dom_element(obj) {
+  return !!(obj && obj.nodeType === 1);
+};
